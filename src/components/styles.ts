08eb1b8651a415f8e8bd36467ac9c4d3a3f6762e
Aidev-786 <khowaja.mazhar@hotmail.com>
2025-06-26 14:39:56 +0500

    New Variables Added

--- src/components/styles.ts
+++ src/components/styles.ts
@@ -1,2 +1,3 @@
 export const headingH1 = "text-3xl md:text-4xl text-[#333333] font-medium capitalize leading-snug whitespace-nowrap";
 export const headingH1Nc = "text-3xl md:text-4xl text-[#333333] font-medium leading-snug whitespace-nowrap"
+export const normalFont = 'text-[13px]'
